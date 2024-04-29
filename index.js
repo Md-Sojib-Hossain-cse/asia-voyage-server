652578b0c23db507f55b5941ae2e976286d28962
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-04-29 12:24:03 +0600

    put api successfully updated expected document

--- index.js
+++ index.js
@@ -35,40 +35,69 @@ async function run() {
 
 
         //get pre defined tourist spots for homepage
-        app.get("/touristSpots" , async(req , res) => {
+        app.get("/touristSpots", async (req, res) => {
             const result = await touristSpotsCollection.find().toArray();
             res.send(result)
         })
 
         //create tourist spot details
-        app.post("/addTouristSpot" , async(req , res) => {
+        app.post("/addTouristSpot", async (req, res) => {
             const touristSpotDetails = req.body;
             const result = await allUsersTouristSpots.insertOne(touristSpotDetails);
             res.send(result);
         })
 
         //get all user created tourist spots
-        app.get("/allTouristSpot" , async(req , res) => {
+        app.get("/allTouristSpot", async (req, res) => {
             const result = await allUsersTouristSpots.find().toArray();
             res.send(result);
         })
 
         //get single tourist spot
-        app.get("/allTouristSpot/:id" , async(req , res) => {
+        app.get("/allTouristSpot/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await allUsersTouristSpots.findOne(query);
             res.send(result);
         })
 
 
         //get single user added data 
-        app.get("/myList/:email" , async(req , res) => {
-            const email = {userEmail : req.params.email};
-            const result =await allUsersTouristSpots.find(email).toArray();
+        app.get("/myList/:email", async (req, res) => {
+            const email = { userEmail: req.params.email };
+            const result = await allUsersTouristSpots.find(email).toArray();
             res.send(result);
         })
 
+
+        //update a single tourist spot
+        app.put("/myList/update/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedSpotDetails = req.body;
+
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+
+            const updateDoc = {
+                $set: {
+                    userName: updatedSpotDetails.userName,
+                    userEmail: updatedSpotDetails.userEmail,
+                    touristSpotName: updatedSpotDetails.touristSpotName,
+                    countryName: updatedSpotDetails.countryName,
+                    location: updatedSpotDetails.location,
+                    photoUrl: updatedSpotDetails.photoUrl,
+                    averageCost: updatedSpotDetails.averageCost,
+                    seasonality: updatedSpotDetails.seasonality,
+                    travelTime: updatedSpotDetails.travelTime,
+                    visitorPerYear: updatedSpotDetails.visitorPerYear,
+                    shortDescription: updatedSpotDetails.shortDescription,
+                },
+            };
+
+            const result = await allUsersTouristSpots.updateOne(filter , updateDoc , options);
+            
+            res.send(result);
+        })
 
 
 
